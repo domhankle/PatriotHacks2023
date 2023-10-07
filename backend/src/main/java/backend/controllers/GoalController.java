2854ff2814f2431de634f6c55bc47190d5157143
Victor <vlondon2@gmu.edu>
2023-10-07 11:56:15 -0400

    matching dev branch

--- backend/src/main/java/backend/controllers/GoalController.java
+++ backend/src/main/java/backend/controllers/GoalController.java
@@ -1,4 +1,4 @@
-package backend;
+package main.java.backend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.ArrayList;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -16,6 +17,7 @@ import software.amazon.awssdk.services.dynamodb.model.*;
 
 @RestController
 @RequestMapping("/goals")
+@CrossOrigin(origins = "http://localhost:4200")
 public class GoalController {
 
     @PostMapping("/prompt")
